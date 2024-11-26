34310371edeb5d040572fb01dd6a1d86cbde9be7
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-11-26 13:31:13 -0500

    Add a testing prompt and an image

--- memory-card-game/src/App.js
+++ memory-card-game/src/App.js
@@ -1,10 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+const imgUrl = "https://static.wikia.nocookie.net/naruto/images/d/d6/Naruto_Part_I.png"
+
 
 function App() {
+  function startGame() {
+    prompt('Start game?');
+  }
+  startGame();
+
+  function getCharacterCard() {
+
+  }
   return (
     <div className="App">
       <header className="App-header">
+      <img src={imgUrl}/>
       </header>
     </div>
   );
