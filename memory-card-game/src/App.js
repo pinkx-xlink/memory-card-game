9a34a4b357c673a5ec624d0a1fb6e2395f763613
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-11-26 14:23:48 -0500

    Create an image array

--- memory-card-game/src/App.js
+++ memory-card-game/src/App.js
@@ -4,6 +4,9 @@ import React, { useEffect, useState } from 'react';
 const narutoImgUrl = "https://static.wikia.nocookie.net/naruto/images/d/d6/Naruto_Part_I.png"
 const sasukeImgUrl = "https://static.wikia.nocookie.net/naruto/images/2/21/Sasuke_Part_1.png"; 
 const tsunadeImgUrl = "https://static.wikia.nocookie.net/naruto/images/b/b3/Tsunade_infobox2.png";
+
+const imgArray = [narutoImgUrl, sasukeImgUrl, tsunadeImgUrl];
+
 function App() {
   function startGame() {
     console.log('Starting game...');
