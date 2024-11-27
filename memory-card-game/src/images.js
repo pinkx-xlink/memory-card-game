392403f8697228aba63e0312dabbf35d89e15d8b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-11-27 17:15:09 -0500

    Delete lines 27-39 of commented out images

--- memory-card-game/src/images.js
+++ memory-card-game/src/images.js
@@ -25,19 +25,6 @@ const Gallery = () => {
       
 //);
   
-  // const narutoImgUrl = "https://static.wikia.nocookie.net/naruto/images/d/d6/Naruto_Part_I.png"
-  // const sasukeImgUrl = "https://static.wikia.nocookie.net/naruto/images/2/21/Sasuke_Part_1.png"; 
-  // const tsunadeImgUrl = "https://static.wikia.nocookie.net/naruto/images/b/b3/Tsunade_infobox2.png";
-  // const shikamaruImgUrl = "https://static.wikia.nocookie.net/naruto/images/4/44/Shikamaru_Part_I.png";
-  // const zabuzaImgUrl = "https://static.wikia.nocookie.net/naruto/images/3/37/Zabuza_Momochi.png";
-  // const asumaImgUrl = "https://static.wikia.nocookie.net/naruto/images/7/7c/Asuma.png";
-  // const gamatatsuImgUrl = "https://static.wikia.nocookie.net/naruto/images/8/81/Gamatatsu.png";
-  // const kankuroImgUrl = "https://static.wikia.nocookie.net/naruto/images/7/7d/Kankur%C5%8D1.png";
-  // const hinataImgUrl = "https://static.wikia.nocookie.net/naruto/images/9/97/Hinata.png";
-  // const chojiImgUrl = "https://static.wikia.nocookie.net/naruto/images/7/7d/Ch%C5%8Dji_Akimichi.png";
-  // const kakashiImgUrl = "https://static.wikia.nocookie.net/naruto/images/2/27/Kakashi_Hatake.png";
-  // const gaaraImgUrl = "https://static.wikia.nocookie.net/naruto/images/2/20/Gaara_in_Part_I.png";
-
   const imgArray = [
     {id: 'naruto', src: 'https://static.wikia.nocookie.net/naruto/images/d/d6/Naruto_Part_I.png', alt: 'Naruto Uzamaki'},
     {id: 'sasuke', src: 'https://static.wikia.nocookie.net/naruto/images/2/21/Sasuke_Part_1.png', alt: 'Sasuke Uchiha'},
@@ -63,8 +50,6 @@ const Gallery = () => {
     const selected = shuffled.slice(0, 4);
     setSelectedImages(selected);
   }
-
- 
 
   return (
     <>
