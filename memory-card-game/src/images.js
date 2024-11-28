8f5097b5fe51ae336eb3531e9d407a420fa32f5a
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-11-27 19:57:03 -0500

    Troubleshoot img click counter

--- memory-card-game/src/images.js
+++ memory-card-game/src/images.js
@@ -61,7 +61,7 @@ const Gallery = () => {
       <button class="random-img-btn" onClick={getRandomImages}>Get Random img</button>
       <div class="card-container">
         {selectedImages.map((image, id) => (
-          <img onClick={handleImageClick && getRandomImages} key={image.id} src={image.src} alt={`${image.alt}`} class="random-img" />
+          <img onClick={handleImageClick(image.id) && getRandomImages} key={image.id} src={image.src} alt={`${image.alt}`} class="random-img" />
         ))}
       </div> 
     </div>
