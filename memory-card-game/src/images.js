ad5872b7489dfd993d459a8a96169bea963ed370
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-11-29 20:37:28 -0500

    Comment out HandleImgClick on lines 4-21

--- memory-card-game/src/images.js
+++ memory-card-game/src/images.js
@@ -1,24 +1,26 @@
 import React, { useState, useEffect, useRef } from "react";
 // import ClickableImage from "./clickCounter";
 const Gallery = () => {
-  const [clickCount, setClickCount] = useState(0);
-  const HandleImgClick = (event) => {
+  // const [clickCount, setClickCount] = useState(0);
+  // const HandleImgClick = (event) => {
     
-    // let prevCount;
-    setClickCount(prevCount => prevCount + 1);
-    if (clickCount + 1 === 2) {
-      console.log(event.target.alt)
-        alert('Clicked twice!! ): the Hidden Leaf has exploded.');
-        // Add a gameover screen with a pic of Jiraiya Boy
-        setClickCount(0);
-    }
-    console.log(`You clicked ${event.target.alt}`);
-    getRandomImages();
-    // ClickableImage();
-    return (
-      <h2> LOSER :P </h2> 
-   )
-  };
+  //   // let prevCount;
+  //   setClickCount(prevCount => prevCount + 1);
+  //   if (clickCount + 1 === 2) {
+  //     console.log(event.target.alt)
+  //       alert('Clicked twice!! ): the Hidden Leaf has exploded.');
+  //       // Add a gameover screen with a pic of Jiraiya Boy
+  //       setClickCount(0);
+  //   }
+  //   console.log(`You clicked ${event.target.alt}`);
+  //   getRandomImages();
+  //   // ClickableImage();
+  //   return (
+  //     <h2> LOSER :P </h2> 
+  //  )
+  // };
+
+
   const imgArray = [
     {id: 'naruto', src: 'https://static.wikia.nocookie.net/naruto/images/d/d6/Naruto_Part_I.png', alt: 'Naruto Uzamaki'},
     {id: 'sasuke', src: 'https://static.wikia.nocookie.net/naruto/images/2/21/Sasuke_Part_1.png', alt: 'Sasuke Uchiha'},
