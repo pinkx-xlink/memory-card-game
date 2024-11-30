d058e755a60d15ef89fefe4477e43cf01d08d94d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-11-29 20:30:19 -0500

    Revert code - back to receiving alert after ANY 2 img clicks, whether or not they are the same img

--- memory-card-game/src/images.js
+++ memory-card-game/src/images.js
@@ -1,39 +1,23 @@
 import React, { useState, useEffect } from "react";
 // import ClickableImage from "./clickCounter";
 const Gallery = () => {
-  // const [clickCounts, setClickCounts] = useState({});
-  
-  // const handleImageClick = (imageId) => {
-  //   setClickCounts((prevCounts) => ({
-  //     ...prevCounts,
-  //     [imageId]: (prevCounts[imageId] || 0) + 1, }))
-  //     if (clickCounts[imageId] + 1 === 2) {
-  //     alert('You clicked the same image twice! The Hidden Leaf Village has exploded.');
-  //     setClickCounts(0);
-  //     }
-  // };
-  
-    // THIS WORKS KINDA -
-    // but it goes off when any 2 img are clicked, not the same one twice.
-    const HandleImageClick = () => {
-      const [clickCount, setClickCount] = useState('');
-      setClickCount(clickCount => clickCount + 1);
-      if (clickCount + 1 === 2) {
-       alert('Clicked twice!! ): the Hidden Leaf has exploded.');
+  const [clickCount, setClickCount] = useState(0);
+  const HandleImgClick = (event) => {
+    
+    // let prevCount;
+    setClickCount(prevCount => prevCount + 1);
+    if (clickCount + 1 === 2) {
+        alert('Clicked twice!! ): the Hidden Leaf has exploded.');
         // Add a gameover screen with a pic of Jiraiya Boy
         setClickCount(0);
-      }
-      
     }
-    
-    
-    // console.log(`You clicked ${imageId}`);
-    // getRandomImages();
+    console.log(`You clicked ${event.target.alt}`);
+    getRandomImages();
     // ClickableImage();
-    //return (
-      
-//);
-  
+    return (
+      <h2> LOSER :P </h2> 
+   )
+  };
   const imgArray = [
     {id: 'naruto', src: 'https://static.wikia.nocookie.net/naruto/images/d/d6/Naruto_Part_I.png', alt: 'Naruto Uzamaki'},
     {id: 'sasuke', src: 'https://static.wikia.nocookie.net/naruto/images/2/21/Sasuke_Part_1.png', alt: 'Sasuke Uchiha'},
@@ -61,20 +45,14 @@ const Gallery = () => {
   }
 
   return (
-    <>
     <div class="gallery-container">
       <button class="random-img-btn" onClick={getRandomImages}>Get Random img</button>
       <div class="card-container">
-        {selectedImages.map((image, id) => (
-          <img onClick={HandleImageClick && getRandomImages} key={image.id} src={image.src} alt={`${image.alt}`} class="random-img" />
+        {selectedImages.map((image, index) => (
+          <img onClick={HandleImgClick} key={index} src={image.src} alt={`${image.alt}`} class="random-img" />
         ))}
       </div> 
     </div>
-    <div>
-   
-  </div>
-  </>
   );
 };
-
 export default Gallery;
