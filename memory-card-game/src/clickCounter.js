c0e378ac0febac1d297330833843b58ab2dd6663
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2024-11-29 22:03:49 -0500

    Comment out ClickableImage() to make room for a different take on the function

--- memory-card-game/src/clickCounter.js
+++ memory-card-game/src/clickCounter.js
@@ -1,20 +1,22 @@
 import React, { useState } from "react";
 
-const ClickableImage = (event) => {
-    const [clickCount, setClickCount] = useState(0);
+const Photo = ({ src, alt }) 
+// first rendition
+// const ClickableImage = (event) => {
+//     const [clickCount, setClickCount] = useState(0);
     
-    const handleClick = () => {
-        // let prevCount;
-        setClickCount(prevCount => prevCount + 1);
-        if (clickCount + 1 === 2) {
-            alert('Clicked twice!! ): the Hidden Leaf has exploded.');
-            // Add a gameover screen with a pic of Jiraiya Boy
-            setClickCount(0);
-        }
-    };
-    return (
-       <h2> LOSER :P </h2> 
-    )
-}
+//     const handleClick = () => {
+//         // let prevCount;
+//         setClickCount(prevCount => prevCount + 1);
+//         if (clickCount + 1 === 2) {
+//             alert('Clicked twice!! ): the Hidden Leaf has exploded.');
+//             // Add a gameover screen with a pic of Jiraiya Boy
+//             setClickCount(0);
+//         }
+//     };
+//     return (
+//        <h2> LOSER :P </h2> 
+//     )
+// }
 
 export default ClickableImage;
